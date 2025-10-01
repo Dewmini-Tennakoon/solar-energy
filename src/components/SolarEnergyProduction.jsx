4c207425f4f37851f8518ef9790d185cae0a4768
Dewmini Tennakoon <dewminitennakoondm@gamil.com>
2025-10-01 18:07:25 +0530

    #2 update

--- src/components/SolarEnergyProduction.jsx
+++ src/components/SolarEnergyProduction.jsx
@@ -2,7 +2,8 @@ const SolarEnergyProduction = () => {
 
     return (
         <section>
-            
+            <h2 className=" ">Solar Energy Production</h2>
+            <p>Daily energy output for the past 7 days</p>
         </section>
     )
 
