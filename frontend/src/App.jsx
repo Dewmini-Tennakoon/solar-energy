f4a9205461364ebbe9594fa894a862e1621691c8
Dewmini Tennakoon <dewminitennakoondm@gmail.com>
2025-12-04 20:02:14 +0530

    commit fullstack

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from "./components/Navigation/Navigation";
+import HeroSection from "./components/HeroSection/HeroSection";
+import SolarEnergyProduction from "./components/SolarEnergyProduction";
+ import EnergyProductionCards from "./components/EnergyProductionCards";
+export default function App() {
+  return (
+    <>
+      <Navigation />
+      <HeroSection />
+      <SolarEnergyProduction />
+    </>
+  );
+  
+}
+
