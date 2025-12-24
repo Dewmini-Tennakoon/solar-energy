43016d1741bc6049ac702c6510618e8fea1b9b8e
Dewmini Tennakoon <dewminitennakoondm@gmail.com>
2025-12-24 17:37:52 +0530

    Frontend: dashboard UI, charts, auth, sidebar

--- frontend/src/pages/dashboard/dashboard.page.jsx
+++ frontend/src/pages/dashboard/dashboard.page.jsx
@@ -1,9 +1,28 @@
-import Navigation from "@/components/Navigation/Navigation";
+import { useGetEnergyGenerationRecordsBySolarUnitQuery } from "@/lib/redux/query";
+import DataCard from "./components/DataCard";
+import DataChart from "./components/DataChart";
+import { useUser } from "@clerk/clerk-react";
 
 const DashboardPage = () => {
+  const { user } = useUser();
+
+  const solarUnitId = "68f4f2cef076449e2049b9c1";
+
   return (
-    <main className="px-12">
-      <h1 className="text-4xl font-bold text-foreground">Alice's House</h1>
+    <main className="mt-4">
+      <h1 className="text-4xl font-bold text-foreground">{user?.firstName}'s House</h1>
+      <p className="text-gray-600 mt-2">
+        Welcome back to your Solar Energy Production Dashboard
+      </p>
+      <div className="mt-8">
+        <DataCard
+          solarUnitId={solarUnitId}
+          title="Last 7 Days Energy Production"
+        />
+      </div>
+      <div className="mt-8">
+        <DataChart solarUnitId={solarUnitId} />
+      </div>
     </main>
   );
 };
